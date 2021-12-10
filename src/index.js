9813fe268a73688a93fea97a7a31702283cd01c8
NataliaPoletaeva <natfly799@gmail.com>
2021-12-10 19:26:46 +0500

    Fix linters'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import './style.css';
 import { taskForm, addTask, check } from './taskInfo.js';
-import clearAll from './clear-all.js'
-import { save, render } from './localStorage.js'
+import clearAll from './clear-all.js';
+import { save, render } from './localStorage.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  save();
-  render();
-});
-
-document.querySelector('.add-arrow').addEventListener('click', () => {
-  addTask(event);
   save();
   render();
 });
@@ -19,5 +13,5 @@ taskForm.addEventListener('submit', addTask);
 const unorderedList = document.querySelector('[data-todos]');
 unorderedList.addEventListener('change', check);
 
-const removeAll = document.querySelector('.clear')
+const removeAll = document.querySelector('.clear');
 removeAll.addEventListener('click', clearAll);
